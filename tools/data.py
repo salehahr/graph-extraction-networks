b84de0e78061b2d7b527567353644aa5205bd09a
SAR <salehahr@users.noreply.github.com>
2022-01-03 15:30:44 +0100

    removed unnecessary functions in tools/data

--- tools/data.py
+++ tools/data.py
@@ -1,53 +1,6 @@
 import os
 
-import skimage.io as io
-from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.data import Dataset
-
-from tools.image import classifier_preview, classify
-
-os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
-
-
-def train_generator(
-    batch_size,
-    train_path,
-    image_folder,
-    mask_folder,
-    target_size,
-    image_color_mode="grayscale",
-    mask_color_mode="grayscale",
-):
-    """Image Data Generator
-    Function that generates batches of data (img, mask) for training
-    from specified folder. Returns images with specified pixel size
-    Does preprocessing (normalization to 0-1)
-    """
-    # no augmentation, only rescaling
-    image_datagen = ImageDataGenerator(rescale=1.0 / 255)
-    mask_datagen = ImageDataGenerator(rescale=1.0 / 255)
-    image_generator = image_datagen.flow_from_directory(
-        train_path,
-        classes=[image_folder],
-        class_mode=None,
-        color_mode=image_color_mode,
-        target_size=target_size,
-        batch_size=batch_size,
-        seed=1,
-    )
-    mask_generator = mask_datagen.flow_from_directory(
-        train_path,
-        classes=[mask_folder],
-        class_mode=None,
-        color_mode=mask_color_mode,
-        target_size=target_size,
-        batch_size=batch_size,
-        seed=1,
-    )
-    train_generator = zip(image_generator, mask_generator)
-    for (img, mask) in train_generator:
-        mask, _ = classify(mask)
-        yield (img, mask)
 
 
 def get_skeletonised_ds(data_path: str, seed: int, is_test: bool = False) -> Dataset:
