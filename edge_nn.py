6bbc72a5d3fbf6c6e91af776f8f98b7896560497
SAR <salehahr@users.noreply.github.com>
2022-02-15 18:56:56 +0100

    updated edge_nn.py for first run - https://wandb.ai/salehah/edge_extraction/runs/2v9g2pcr

--- edge_nn.py
+++ edge_nn.py
@@ -1,19 +1,45 @@
-from model import VGG16
-from tools import Config
+from tools import TestType, get_eedg, get_gedg, run
 
-base_path = "/graphics/scratch/schuelej/sar/tfgraph/"
-name = "edge_nn"
+# user input
+run_name = "edge_nn_16f"
+start_image_id = 1
 
 if __name__ == "__main__":
-    conf = Config("config.yaml")
-    network = conf.network.edge_extraction
+    # configs
+    data_config, run_config = run.get_configs("config.yaml", run_name)
+    # run.check_weights(run_config)
 
-    n_filters = 4
+    # generate data
+    graph_data = get_gedg(data_config)
+    num_images = len(graph_data[run_config.run_type])
 
-    # build model
-    edge_nn = VGG16(
-        input_size=(*conf.img_dims, network.input_channels),
-        n_filters=n_filters,
-        pretrained_weights=None,
-    )
-    edge_nn.build()
+    # loop over all images
+    old_run_id = run_config.run_id
+    edge_nn = None
+
+    for i in range(start_image_id, num_images):
+        # init new run
+        do_resume = False if i == 0 else "must"
+        run.start(run_config, resume=do_resume, reinit=True, _id=old_run_id)
+
+        # generate node combinations
+        edge_data = get_eedg(data_config, graph_data, step_num=i)
+
+        # init model/reload model on resumed run
+        edge_nn = run.load_model(data_config, run_config, model_=edge_nn)
+
+        # train and save for next run
+        run.train(edge_nn, edge_data)
+        old_run_id = run.save(edge_nn, run_config.model_filename)
+
+        # predict for 5 images
+        if i < 5:
+            run.predict(
+                edge_nn,
+                edge_data[TestType.VALIDATION],
+                max_pred=7,
+                only_adj_nodes=True,
+            )
+
+        # terminate run
+        run.end()
