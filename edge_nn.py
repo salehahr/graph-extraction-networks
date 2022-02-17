3a345ed2bccd5f4ef0b5f9fc80b028ccacb48021
SAR <salehahr@users.noreply.github.com>
2022-02-17 15:41:24 +0100

    updated edge_nn.py and configs for next run

--- edge_nn.py
+++ edge_nn.py
@@ -1,47 +1,36 @@
+import math
+
 from tools import TestType, get_eedg, get_gedg, run
 
 # user input
-run_name = "edge_nn_16f"
-start_image_id = 1
+run_name = "edge_nn"
+do_resume = False  # "must"
+do_reinit = False if not do_resume else True
+
+max_num_images = 50
 
 if __name__ == "__main__":
     # configs
     data_config, run_config = run.get_configs("config.yaml", run_name)
-    # run.check_weights(run_config)
 
     # generate data
-    graph_data = get_gedg(data_config, batch_size=1)
-    num_images = len(graph_data[run_config.run_type])
+    graph_data = get_gedg(data_config)
+    edge_data = get_eedg(data_config, run_config, graph_data)
 
-    # loop over all images
-    old_run_id = run_config.run_id
-    edge_nn = None
+    # init new run
+    run.start(run_config, resume=do_resume, reinit=do_reinit, _id=run_config.run_id)
 
-    for i in range(start_image_id, num_images):
-        # init new run
-        do_resume = False if i == 0 else "must"
-        run.start(run_config, resume=do_resume, reinit=True, _id=old_run_id)
+    # init model/reload model on resumed run
+    edge_nn = run.load_model(data_config, run_config)
 
-        # generate node combinations
-        edge_data = get_eedg(
-            data_config, run_config.node_pairs_in_batch, graph_data, step_num=i
-        )
+    # train and save for next run
+    # one step contains n = run_config.batch_size images
+    steps = math.ceil(max_num_images / run_config.batch_size)
+    run.train(edge_nn, edge_data, steps_in_epoch=steps)
+    run.save(edge_nn, run_config.model_filename)
 
-        # init model/reload model on resumed run
-        edge_nn = run.load_model(data_config, run_config, model_=edge_nn)
+    # predict for 5 combinations
+    run.predict(edge_nn, edge_data[TestType.VALIDATION], max_pred=5)
 
-        # train and save for next run
-        run.train(edge_nn, edge_data)
-        old_run_id = run.save(edge_nn, run_config.model_filename)
-
-        # predict for 5 images
-        if i < 5:
-            run.predict(
-                edge_nn,
-                edge_data[TestType.VALIDATION],
-                max_pred=7,
-                only_adj_nodes=True,
-            )
-
-        # terminate run
-        run.end()
+    # terminate run
+    run.end()
