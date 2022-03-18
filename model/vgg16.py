3a4dca6abef0c0e8806b1201a2d2a623f89d865e
SAR <salehahr@users.noreply.github.com>
2022-03-18 15:02:33 +0100

    add more metrics to be logged

--- model/vgg16.py
+++ model/vgg16.py
@@ -123,10 +123,22 @@ class VGG16(Model):
         self.summary()
 
     def recompile(self, **kwargs):
+        metrics = [
+            tf.keras.metrics.TruePositives(name="tp"),
+            tf.keras.metrics.FalsePositives(name="fp"),
+            tf.keras.metrics.TrueNegatives(name="tn"),
+            tf.keras.metrics.FalseNegatives(name="fn"),
+            tf.keras.metrics.BinaryAccuracy(name="accuracy"),
+            tf.keras.metrics.Precision(name="precision"),
+            tf.keras.metrics.Recall(name="recall"),
+            tf.keras.metrics.AUC(name="auc"),
+            tf.keras.metrics.AUC(name="prc", curve="PR"),  # precision-recall curve
+        ]
+
         self.compile(
             optimizer=Adam(learning_rate=self.learning_rate),
             loss="binary_crossentropy",
-            metrics=["accuracy"],
+            metrics=metrics,
             **kwargs,
         )
 
